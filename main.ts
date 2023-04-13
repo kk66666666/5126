60b16c2d833397be0d71bac6fd30bc478cb2c63f
kk66666666 <115142452+kk66666666@users.noreply.github.com>
2023-04-13 16:47:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+let _ = 0
+basic.forever(function () {
+    input.calibrateCompass()
+    _ = input.compassHeading()
+    if (315 < _ || _ < 45) {
+        basic.showString("N")
+    } else {
+        basic.showString("")
+    }
+})
